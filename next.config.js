10eac0d338ff292df21f5af295feaf371225b451
ait0rtg <aitor.tendero@gmail.com>
2026-04-27 15:16:42 +0200

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['openai'],
-  },
-  images: {
-    domains: [],
-  },
+  serverExternalPackages: ['openai'],
 }
 
 module.exports = nextConfig
